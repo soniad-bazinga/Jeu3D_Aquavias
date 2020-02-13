5844150c14c4192d6d8f2bb680d9c7764b7dec97
leskerrr <franck.kindia@gmail.com>
2020-02-13 23:30:31 +0100

    Damn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,7 @@ import org.json.simple.parser.ParseException;
 
 public class Main {
 
-<<<<<<< HEAD
 	public static void main(String[] args) throws Exception {
-		Level l = new Level(5, 8, 'f', 4586958);
 		Menu m1 = new Menu();
-=======
-	public static void main(String[] args) {
-		try {
-			Level lvl2 = new Level(1);
-			InputsWindow iw = new InputsWindow(lvl2);
-			lvl2.affiche();
-			//lvl2.play();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
->>>>>>> 71939cc7fc1fc0570db21e6f7ab1400b27d18074
 	}
 }

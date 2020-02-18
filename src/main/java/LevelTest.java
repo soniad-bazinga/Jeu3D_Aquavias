64004ea9a05541c0670b022dbb1cff6e469be61f
sdahma28 <so.prime2000@gmail.com>
2020-02-18 19:53:33 +0100

    added some unit tests

---
+++ src/main/java/LevelTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelTest {
+
+    @org.junit.jupiter.api.Test
+    void clearScreen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasWon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testUpdate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFull() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isInTab() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isFull() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connected() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPiecePos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInputInt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printBlocker() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createLevel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void movePointer() {
+    }
+}

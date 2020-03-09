7cf7ddfe3493db7f952d690ce93f53b8d43c6e32
Elsa Duarte <elsasantosduarte@gmail.com>
2020-03-09 20:33:45 +0100

    correction of menu(starts at 0)

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
     final File folder = new File("levels/");
     Scanner player;
     int selected_option  = 0;
-    int selected_level = 1;
+    int selected_level = 0;
     int level_number = 0;
     boolean selecting_level = false;
     boolean closed = false;
@@ -40,7 +40,7 @@ public class Menu {
     }
 
     public void printFolderLevels(final File folder) throws Exception {
-        int i = 1;
+        int i = 0;
         for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
             if (!fileEntry.getName().equals("id.json")) {
                 if(i == selected_level) System.out.print(ANSI_SELECTED);
@@ -64,8 +64,8 @@ public class Menu {
     	//dir=1 on descend dans le menu, -1 on monte
         if(!selecting_level) return;
         if(dir == 1 && selected_level<(level_number-1)) selected_level++;
-        if(dir == -1 && selected_level > 1) selected_level--;
-        if(!(dir == -1 && selected_level == 0) && !(dir == 1 && selected_level == level_number))
+        if(dir == -1 && selected_level > 0) selected_level--;
+        if(!(dir == -1 && selected_level == -1) && !(dir == 1 && selected_level == level_number))
         afficheLevels();
     }
 

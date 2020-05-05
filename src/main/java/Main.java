21ae5f96eb4b1faa4bde9f5693c9d11a2e7c68b7
Eugène VALTY <eugenevalty@gmail.com>
2020-05-05 23:28:54 +0200

    modifié le niveau 2 pour corriger l'écoulement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import org.json.simple.parser.ParseException;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Level level = new Level(2);
+		Level level = new Level(0);
 		level.new_update();
 		level.affiche();
 		View v = new View(level);
